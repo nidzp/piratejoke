7db62367c6eac7c1a5fc0143e0be01c211657e1b
Nikola <95056678+nidzp@users.noreply.github.com>
2025-10-27 17:08:02 +0100

    v1

--- src/routes/movieRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const movieController = require('../controllers/movieController');
-
-// GET /api/movies/search/:title - Search for a movie and return streaming sources
-router.get('/movies/search/:title', movieController.searchMovie);
-
-module.exports = router;
